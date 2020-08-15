a9e25ff0aa906956cd10c7f9250c393fd5a241fb
mhumam <muhamadhumamm17@gmail.com>
2020-08-15 15:03:18 +0700

    enhancement code in list post component

--- src/pages/Post/Index.js
+++ src/pages/Post/Index.js
@@ -9,39 +9,11 @@
 import React from 'react';
 import { Divider, Breadcrumb } from 'antd';
 import { Link } from 'react-router-dom';
-import { getRequest } from '../../utilities/RequestService';
-import { Spin, ListPost, UserDetail } from '../../components/Base/Index';
-import { api } from '../../config/Endpoint';
+import { ListPost, UserDetail } from '../../components/Base/Index';
 import { HomeOutlined } from '@ant-design/icons';
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			isLoading: false,
-			postList: []
-		}
-	}
-
-	async componentDidMount() {
-		/* get userId from uri */
-		const userId = this.props.match.params.userid;
-		/* set url */
-		const url = api.url.posts.list + "/?userId=" + userId;
-		/* callback after calling service */
-		const callback = async (response) => {
-			const postList = (response) ? response : [];
-			/* set postList and hide loader */
-			await this.setState({ postList, isLoading: false });
-		}
-		/* call loader */
-		await this.setState({ isLoading: true });
-		/* call service */
-		await getRequest(url, callback);
-	}
-
 	render() {
-		const { postList, isLoading } = this.state;
 		const userId = this.props.match.params.userid;
 		return (
 			<div className="site-layout-background" style={{ padding: 24, paddingTop: 0, minHeight: 380 }}>
@@ -58,9 +30,7 @@ class App extends React.Component {
 					</Breadcrumb>
 					<UserDetail userId={userId} />
 					<Divider>List Post</Divider>
-					<Spin isLoading={isLoading}>
-						<ListPost dataSource={postList} />
-					</Spin>
+					<ListPost userId={userId} />
 				</div>
 			</div>
 		)
