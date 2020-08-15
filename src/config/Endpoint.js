207075e87011dbcd78f7d53a64d6b76a504d764a
mhumam <muhamadhumamm17@gmail.com>
2020-08-15 12:05:51 +0700

    initialize endpoint base

---
+++ src/config/Endpoint.js
@@ -0,0 +1,17 @@
+/**
+ * @author Muhamad Humam
+ * @email muhamadhumamm17@gmail.com
+ * @create date 2020-08-15 12:03:57
+ * @modify date 2020-08-15 12:03:57
+ * @desc API Endpoint Base
+ */
+
+const endpoint = "https://jsonplaceholder.typicode.com/";
+
+export const api = {
+    url: {
+        users: {
+            list: endpoint + "users"
+        }
+    }
+}
