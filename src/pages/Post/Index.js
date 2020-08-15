82874571ebe8e4058fcdade2e16f27588c49923e
mhumam <muhamadhumamm17@gmail.com>
2020-08-15 12:06:35 +0700

    initialize post page

---
+++ src/pages/Post/Index.js
@@ -0,0 +1,22 @@
+/**
+ * @author Muhamad Humam
+ * @email muhamadhumamm17@gmail.com
+ * @create date 2020-08-15 09:28:36
+ * @modify date 2020-08-15 09:28:36
+ * @desc Post Page
+ */
+
+import React from 'react';
+
+class App extends React.Component {
+
+	render() {
+		return (
+			<div className="site-layout-background" style={{ padding: 24, paddingTop: 0, minHeight: 380 }}>
+				Post Page
+			</div>
+		)
+	}
+}
+
+export default App;
