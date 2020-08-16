c40dda76f2268e9a645269cec663dafe190a0edc
mhumam <muhamadhumamm17@gmail.com>
2020-08-16 21:38:59 +0700

    add Alert, InputText, TextArea in Base

--- src/components/Base/Index.js
+++ src/components/Base/Index.js
@@ -6,3 +6,7 @@ export { default as AlbumsList } from '../AlbumsList';
 export { default as PhotosList } from '../PhotosList';
 export { default as PostDetails } from '../PostDetails';
 export { default as CommentsList } from '../CommentsList';
+
+export { default as Alert } from '../Alert';
+export { default as InputText } from '../Input/InputText';
+export { default as TextArea } from '../Input/TextArea';
